e6796e90bf6dbc61aa6bb5c049ab21221da3d74c
Juha-Matti Juntunen <loop0022@hotmail.com>
2026-08-02 22:46:25 +0300

    Node API server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/tapahtumat", async (req, res) => {
+
+  try {
+
+    const vastaus = await fetch(
+      "https://tapahtumat.tampere.fi/api/collection/634844c32f41a024ee51a234/content?lang=fi&country=FI&e=24.02&n=61.71&s=61.36&w=23.54&sort=startDate"
+    );
+
+    const data = await vastaus.json();
+
+    res.json(data);
+
+  } catch (error) {
+
+    console.error(error);
+    res.status(500).send("Virhe tapahtumissa");
+
+  }
+
+});
+
+app.listen(3000, () => {
+  console.log("Serveri käynnissä portissa 3000");
+});
